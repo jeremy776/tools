55527919fdd461cccbfa7ebd07f476b22cbd5417
jeremy776 <jeremy.kusuma2018@gmail.com>
2023-03-12 01:03:47 +0700

    feature: statistic web

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBNfsXdU_Ncy3S9xW8Ua1MsdoZa6VFOVdI",
+    authDomain: "uptime-5033a.firebaseapp.com",
+    databaseURL: "https://uptime-5033a-default-rtdb.firebaseio.com",
+    projectId: "uptime-5033a",
+    storageBucket: "uptime-5033a.appspot.com",
+    messagingSenderId: "389680937363",
+    appId: "1:389680937363:web:47d13b32fd9ca5fbf55066",
+    measurementId: "G-0STJPTXBPX"
+};
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
